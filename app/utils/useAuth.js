9c146d344e0a724fe2ab2e09126df339f7945e3b
hmgcfr1331 <hmgcfr2501@gmail.com>
2025-08-11 11:56:51 +0900

    フロントエンド　コミット

---
+++ app/utils/useAuth.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react"
+import { useRouter } from "next/navigation"
+import { jwtVerify } from "jose"
+
+const useAuth = () => {
+    const [loginUserEmail, setLoginUserEmail] = useState("")
+    const router = useRouter()
+
+    useEffect(() => {
+        const checkToken = async () => {
+            const token = localStorage.getItem("token")
+            if (!token) {
+                router.push("/user/login")
+            }
+            try{
+                const secretKey = new TextEncoder().encode("next-market-app-book")
+                const decodeJwt = await jwtVerify(token, secretKey)
+                setLoginUserEmail(decodeJwt.payload.email)
+            }catch{
+                router.push("/user/login")
+            }
+        }
+        checkToken()
+    }, [router])
+
+    return loginUserEmail
+}
+
+export default useAuth
--- middleware.js
+++ middleware.js
@@ -2,7 +2,8 @@ import { NextResponse } from "next/server"
 import { jwtVerify } from "jose"
 
 export async function middleware(request){
-    const token = await request.header.get("Authorizetion")?.spilit("")[1]
+    const token = await request.headers.get("Authorization")?.split(" ")[1]
+    //const token = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InRhcm9AZ21haWwuY29tIiwiZXhwIjoxNzU0MjkzNzgyfQ.DLns7FYfgYyeHCr3s56T7qEtRHfYcXuo-XD9WMc_Px0"
     if(!token){
         return NextResponse.json({message: "トークンがありません"})
     }
