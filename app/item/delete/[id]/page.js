270cd5d3a1a43fe7f2beb1af6609b2c9aa9a8e60
hmgcfr1331 <hmgcfr2501@gmail.com>
2025-08-11 12:01:26 +0900

    buildできないbug対応-1

--- app/item/delete/[id]/page.js
+++ app/item/delete/[id]/page.js
@@ -3,7 +3,6 @@ import { useState, useEffect, use } from "react"
 import { useRouter } from "next/navigation"
 import Image from "next/image"
 import useAuth from "../../../utils/useAuth"
-import { log } from "console"
 
 const DeleteItem = (context) => {
     const [title, setTitle] = useState("")
